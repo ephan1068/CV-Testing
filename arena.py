e15bbea44867b4164e31d20a7caf3bb14e85156b
ephan1068 <ericphan c:; cd 'c:\ephan\CV Testing'; & 'C:\Users\ericp\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\ericp\.vscode\extensions\ms-python.python-2021.12.1559732655\pythonFiles\lib\python\debugpy\launcher' '53236' '--' 'c:\ephan\CV Testing\test.py' 2409@gmail.com>
2022-02-09 14:28:05 -0500

    add obstacle drawing

--- arena.py
+++ arena.py
@@ -34,8 +34,58 @@ def processMarkers(frame, marker_list, H):
             #Add a green arrowed line
             frame = cv2.arrowedLine(frame,(int(x.corner1[0]), int(x.corner1[1])),(int(x.corner2[0]), 
             int(x.corner2[1])),(0, 255, 0),2,tipLength= .4)
+    frame = createObstacles(frame,H)
     return frame
-    
+def createObstacles(frame,H):
+    #
+    #50cm -y
+    #20cm -x
+    #.55 is the x-coord of the obj and 
+    possible_x = [0.55, 1.5, 2.3]
+    possible_y = [0.25,0.75,1.25]
+   
+    x_length = 0.2
+    y_length = 0.5
+    point1 = np.float32(np.array([[[possible_x[1], possible_y[1]]]]))
+    point2 = np.float32(np.array([[[possible_x[1] + x_length, possible_y[1] + y_length]]]))
+    point3 = np.float32(np.array([[[1.6, 0.50]]]))
+    inverse_matrix = np.linalg.pinv(H)
+    transformed_1 = cv2.perspectiveTransform(point1, inverse_matrix)
+    transformed_2 = cv2.perspectiveTransform(point2, inverse_matrix)
+    text = cv2.perspectiveTransform(point3, inverse_matrix)
+    print(transformed_1)
+    print(transformed_2)
+    frame = cv2.rectangle(frame,(int(transformed_1[0,0,0]),int(transformed_1[0,0,1])),
+        (int(transformed_2[0,0,0]),int(transformed_2[0,0,1])),(185,146,68),3)
+    frame = cv2.putText(frame, 'S', (int(text[0,0,0]),int(text[0,0,1])), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,0,0), 2, cv2.LINE_AA)
+    print("not implemented yet")
+
+    point1 = np.float32(np.array([[[possible_x[2], possible_y[2]]]]))
+    point2 = np.float32(np.array([[[possible_x[2] + x_length, possible_y[2] + y_length]]]))
+    point3 = np.float32(np.array([[[1.6, 0.50]]]))
+    inverse_matrix = np.linalg.pinv(H)
+    transformed_1 = cv2.perspectiveTransform(point1, inverse_matrix)
+    transformed_2 = cv2.perspectiveTransform(point2, inverse_matrix)
+    text = cv2.perspectiveTransform(point3, inverse_matrix)
+    print(transformed_1)
+    print(transformed_2)
+    frame = cv2.rectangle(frame,(int(transformed_1[0,0,0]),int(transformed_1[0,0,1])),
+        (int(transformed_2[0,0,0]),int(transformed_2[0,0,1])),(185,146,68),3)
+
+
+    point1 = np.float32(np.array([[[possible_x[1], possible_y[0]]]]))
+    point2 = np.float32(np.array([[[possible_x[1] + x_length, possible_y[0] + y_length]]]))
+    point3 = np.float32(np.array([[[1.6, 0.50]]]))
+    inverse_matrix = np.linalg.pinv(H)
+    transformed_1 = cv2.perspectiveTransform(point1, inverse_matrix)
+    transformed_2 = cv2.perspectiveTransform(point2, inverse_matrix)
+    text = cv2.perspectiveTransform(point3, inverse_matrix)
+    print(transformed_1)
+    print(transformed_2)
+    frame = cv2.rectangle(frame,(int(transformed_1[0,0,0]),int(transformed_1[0,0,1])),
+        (int(transformed_2[0,0,0]),int(transformed_2[0,0,1])),(25,177,215),3)
+    return frame     
 
 def translate(marker, H):
     # find the center of the marker in pixels
